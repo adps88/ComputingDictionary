2103fd1c92eabd645b6fe0f1765ecfa499c63304
adps88 <adps88@gmail.com>
2017-12-03 10:43:35 +0000

    Re-written to use jQuery

--- js/main.js
+++ js/main.js
@@ -1,46 +1,25 @@
 "use strict";
+
 function searchWord(){
-  var dict = document.getElementById("dictionary");
-  var defDivs = dict.children;
+  $(".definition").each(function(index, definitionDiv){
+    var show = divContainsWord(definitionDiv);
 
-  if(document.getElementById("search").value.length >=3){
-
-    for(var i=1; i<defDivs.length; i++){
-      var show = divContainsWord(defDivs[i]);
-
-      if(show==true){
-        defDivs[i].style.display = "block";
-      } else {
-        defDivs[i].style.display = "none";
-      }
+    if(show==true){
+      definitionDiv.style.display = "block";
+    } else {
+      definitionDiv.style.display = "none";
     }
-  } else {
-    for(var i=1; i<defDivs.length; i++){
-      defDivs[i].style.display = "block";
-    }
-  }
+  });
 }
 
-function divContainsWord(defDiv){
-  var divs = defDiv.children;
-  var word = divs[0].innerHTML.toLowerCase();
-  var def = divs[1].innerHTML.toLowerCase();
+function divContainsWord(definitionDiv){
+  var rawText = $(definitionDiv).text().toLowerCase();
+  var searchWord = $('#search').val().toLowerCase();
 
-  if(stringContainsWord(word) || stringContainsWord(def)){
-    return true;
+  if(rawText.indexOf(searchWord)<0){
+    return false;
   }
-  return false;
-}
-
-function stringContainsWord(word){
-  var searchWord = document.getElementById("search").value.toLowerCase();
-
-  for(var i = 0; i < word.length-searchWord.length+1; i++){
-    if (searchWord == word.slice(i, i+searchWord.length)){
-      return true;
-    }
-  }
-  return false;
+  return true;
 }
 
 function generateDefinitions()
@@ -55,7 +34,7 @@ function generateDefinitions()
       var definition = pair[1].trim();
 
       var dictionary = document.getElementById("dictionary");
-      var definitionText = "<div><div>"+word+"</div><div>"+definition+"</div></div>";
+      var definitionText = "<div class='definition'><div>"+word+"</div><div>"+definition+"</div></div>";
 
       dictionary.innerHTML = dictionary.innerHTML + definitionText
     }
