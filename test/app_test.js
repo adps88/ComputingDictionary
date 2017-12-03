19610be767864550786405fb5fc17958befb6952
Matthew Shaw <shaw500@gmail.com>
2017-12-03 21:52:20 +0800

    Refactor tests

--- test/app_test.js
+++ test/app_test.js
@@ -9,33 +9,41 @@ describe('app test', function () {
     beforeEach(() => server = require('../server/app')());
     afterEach(() => server.close());
 
-    it('responds to /hello', function testSlash(done) {
-        request(server).get('/hello').expect(200).end((err, res) => {
-            expect(res.body.message).to.equal("Hello World!");
-            done();
-        });
-    });
+    describe('the basics', function () {
 
-    it('responds to /goodbye', function testSlash(done) {
-        request(server).get('/goodbye').expect(200).end((err, res) => {
-            expect(res.body.message).to.equal("Cya!");
-            done();
-        });
-    });
-
-    it('responds to /definitions', function testSlash(done) {
-        request(server).get('/definitions').expect(200).end((err, res) => {
-            expect(res.body).to.deep.equal({
-                "Binary": "1's and 0's representing all data within a computer system.",
-                "ASCII": "The coding system a computer uses to represent characters.",
-                "Computing": "The subject about computers and computery stuff."
+        it('responds to /hello', function testSlash(done) {
+            request(server).get('/hello').expect(200).end((err, res) => {
+                expect(res.body.message).to.equal("Hello World!");
+                done();
             });
-            done();
         });
+
+        it('responds to /goodbye', function testSlash(done) {
+            request(server).get('/goodbye').expect(200).end((err, res) => {
+                expect(res.body.message).to.equal("Cya!");
+                done();
+            });
+        });
+
+        it('404 everything else', function testPath(done) {
+            request(server).get('/foo/bar').expect(404, done);
+        });
+
     });
 
-    it('404 everything else', function testPath(done) {
-        request(server).get('/foo/bar').expect(404, done);
+    describe('now down to business', function(){
+
+        it('responds to /definitions', function testSlash(done) {
+            request(server).get('/definitions').expect(200).end((err, res) => {
+                expect(res.body).to.deep.equal({
+                    "Binary": "1's and 0's representing all data within a computer system.",
+                    "ASCII": "The coding system a computer uses to represent characters.",
+                    "Computing": "The subject about computers and computery stuff."
+                });
+                done();
+            });
+        });
+
     });
 
 });
