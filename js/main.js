5a5c3346215f8a8ce5ee711f7eb7bb5bb81d69bf
adps88 <adps88@gmail.com>
2017-12-03 14:46:33 +0000

    Bug Fix - Now searches word aswell as definition.

--- js/main.js
+++ js/main.js
@@ -3,38 +3,47 @@
 function searchWord(){
   $('.highlighted').contents().unwrap();
 
-  $(".definition").each(function(index, definitionDiv){
-    var show = dictionaryItemContainsWord(definitionDiv, $("#search").val());
+  if($("#search").val().length>0)
+  {
+    $(".definition").each(function(index, definitionDiv){
+      var show = dictionaryItemContainsWord(definitionDiv, $("#search").val());
 
-    if(show==true){
+      if(show==true){
+        definitionDiv.style.display = "block";
+      } else {
+        definitionDiv.style.display = "none";
+      }
+    });
+  } else {
+    $(".definition").each(function(index, definitionDiv){
       definitionDiv.style.display = "block";
-    } else {
-      definitionDiv.style.display = "none";
-    }
-  });
+    });
+  }
 }
 
 function dictionaryItemContainsWord(dictionaryItem, searchWord){
   var wordItem = $(dictionaryItem).children().first();
   var definitionItem = $(dictionaryItem).children().last();
 
-  return divContains(wordItem, searchWord) || divContains(definitionItem, searchWord);
-  
+  var wordItemContains = divContains(wordItem, searchWord);
+  var definitionItemContains = divContains(definitionItem, searchWord);
+
+  return wordItemContains || definitionItemContains;
 }
 
 function divContains(item, searchWord){
   var contains = false;
   var string = item.text();
 
-  for(var i = string.length-searchWord.length; i > 0 ; i--){
+  for(var i = string.length-searchWord.length; i >= 0 ; i--){
     if (searchWord.toLowerCase() == string.slice(i, i+searchWord.length).toLowerCase()){
-      console.log("search word:" + searchWord + " string:" + string);
       contains = true;
-
       string = [string.slice(0, i), "<span class='highlighted'>", string.slice(i)].join("");
       string = [string.slice(0, i+searchWord.length+26), "</span>", string.slice(i+searchWord.length+26)].join("");
     }
+    console.log(1);
   }
+
   $(item).html(string);
 
   return contains;
